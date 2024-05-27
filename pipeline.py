a340633179c5dc435c80a0ddb57727217b1d8fd0
unknown <TylerConger1998@gmail.com>
2024-05-26 19:24:03 -0500

    Update model

--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pandas as pd
+from sklearn.decomposition import FastICA
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,8 +12,12 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was: 0.9318147687570321
-exported_pipeline = KNeighborsClassifier(n_neighbors=20, p=2, weights="uniform")
+# Average CV score on the training set was: 0.9328921655711493
+exported_pipeline = make_pipeline(
+    FastICA(tol=0.1),
+    StandardScaler(),
+    RandomForestClassifier(bootstrap=True, criterion="gini", max_features=0.6500000000000001, min_samples_leaf=9, min_samples_split=20, n_estimators=100)
+)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
